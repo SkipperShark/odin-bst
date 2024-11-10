0270f386cf5f040e7898ae2c8f1f3fd106f2cd59
sharman <sharmanfaridz@gmail.com>
2024-11-10 21:40:54 +0800

    supposed to stop coding, but didnt

--- main.rb
+++ main.rb
@@ -19,21 +19,22 @@ class Tree
     @array = array.sort.uniq
 
     puts "input array post sort+duplicate removal : #{array}\n\n"
-    midpoint = (array.length / 2)
-    mid_ele = array[midpoint]
-    self.root = Node.new(mid_ele, build_tree(array, root), build_tree(array, root))
+    # midpoint = (array.length / 2)
+    # mid_ele = array[midpoint]
+    # self.root = Node.new(mid_ele, build_tree(array, root), build_tree(array, root))
+    self.root = build_tree(array)
   end
 
-  def build_tree(array, node)
-    p "node : #{node}, array : #{array}"
+  def build_tree(array)
+    p "array : #{array}"
     return nil if array.empty? || array.nil?
     return Node.new(array.first, nil, nil) if array.length == 1
 
     midpoint = (array.length / 2)
     mid_ele = array[midpoint]
-    return Node.new(mid_ele, build_tree(array[0...midpoint], node), build_tree(array[(midpoint + 1)..(array.length)], node))
-    # build_tree(array[0...midpoint], node)
-    # build_tree(array[(midpoint + 1)..(array.length)], node)
+    left = build_tree(array[0...midpoint])
+    right = build_tree(array[(midpoint + 1)..(array.length)])
+    Node.new(mid_ele, left, right)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
